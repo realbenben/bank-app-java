81fdb88bc35d7aaa133ae2a373ab34ad564e074d
charly <charly.dewalles@gmail.com>
2022-02-28 12:16:11 +0100

    test requête bdd avec java

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,23 @@
+import java.sql.*;
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("yoyoyoyo");
-		System.out.println("yoyoyofromcharly");
+	public static void main(String[] args) throws SQLException {
+
+		try {
+			Class.forName("org.postgresql.Driver");
+		      Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bank","postgres","SuperUser");
+		      Statement st = conn.createStatement();
+		      ResultSet res = st.executeQuery("SELECT * FROM conseiller");
+		      
+		      while(res.next())
+		    	  
+		    	System.out.println(res.getInt(1) + " " +res.getString(2) + " " + res.getString(3));
+		      	conn.close(); 
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		}
 	}
 
 }
